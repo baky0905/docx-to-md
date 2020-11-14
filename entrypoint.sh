781aa09772598652a2eff84bb3ba28b424975c22
Kristijan Bakaric <kbakaric1@gmail.com>
2020-11-14 16:35:45 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-find . -name '*.md' > filelist.dat
+find . -name '*.docx' > filelist.dat
 
 while read file
 do
-  base_name=$( echo "${file}" | basename "${file}" | sed -e 's/\.md//g')
-  pandoc -f markdown ${file} -o ${PDF_DIR}/${base_name}.pdf
-  git status
-  git diff
+  base_name=$( echo "${file}" | basename "${file}" | sed -e 's/\.docx//g')
+  pandoc -f markdown ${file} -o ${MD_DIR}/${base_name}.md
+  #git status
+  #git diff
 done < filelist.dat
 
 rm filelist.dat
